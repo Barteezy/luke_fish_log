e5ba65b9b75e93cd752896eee5953a73667e74bb
Luke Bartel <bartelluke@gmail.com>
2014-04-12 14:33:40 -0600

    Clean up fish controller

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -2,7 +2,6 @@ class FishController < ApplicationController
   before_action :set_fish, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
-  
   def index
     @fish = Fish.all
   end
@@ -11,50 +10,47 @@ class FishController < ApplicationController
     @fish = Fish.all
   end
 
-
   def new
     @fish = Fish.new
   end
 
   def create
-    @fish = Fish.new(fish_params)
-    @fish.user_id = current_user.id
+    @fish = current_user.fish.build(fish_params)
 
-
-      if @fish.save
-        redirect_to @fish, notice: 'Fish was successfully created.' 
-      else
-        render action: 'new' 
-      end
-    
+    if @fish.save
+      redirect_to @fish, notice: 'Fish was successfully created.' 
+    else
+      render action: 'new' 
+    end
   end
 
-    def update
-      if @fish.update(fish_params)
-          redirect_to @fish, notice: 'Fish was successfully updated.'
-      else
-          render action: 'edit'
-      end
+  def update
+    if @fish.update(fish_params)
+      redirect_to @fish, notice: 'Fish was successfully updated.'
+    else
+      render action: 'edit'
     end
+  end
 
-    def destroy
-      @fish.destroy
-      redirect_to fish_index_url 
-    end
+  def destroy
+    @fish.destroy
+    redirect_to fish_index_url 
+  end
 
   private
-    def set_fish
-      @fish = Fish.find(params[:id])
-    end
 
-    def correct_user
-      if current_user.id != @fish.user.id 
-        flash[:error] = "Not authorized to edit this fish"
-        redirect_to "/fish"
-      end
-    end
+  def set_fish
+    @fish = Fish.find(params[:id])
+  end
 
-    def fish_params
-      params.require(:fish).permit(:image, :location, :species, :lenth, :weight, :bait, :depth, :temperature, :other_notes)
+  def correct_user
+    if @fish.user == current_user
+      flash[:error] = "Not authorized to edit this fish"
+      redirect_to "/fish"
     end
+  end
+
+  def fish_params
+    params.require(:fish).permit(:image, :location, :species, :lenth, :weight, :bait, :depth, :temperature, :other_notes)
+  end
 end
